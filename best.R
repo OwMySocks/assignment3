626da1932fba2758175d4244c621358b82cb30c4
Bexett <rebecca.everett.7@gmail.com>
2014-09-24 21:23:10 -0500

    Began Best

--- best.R
+++ best.R
@@ -3,4 +3,12 @@ best <- function(state, outcome) {
   ## Check that state and outcome are valid
   ## Return hospital name in that state with lowest 30-day death
   ## rate
+  
+  ## open file, store in data frame
+  outcomes <- read.csv("rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv",colClasses="character")
+  ## check if state is in states, outcome in outcome
+  goodstate <- state %in% levels(factor(outcomes$State))
+  goodoutcome <- outcome %in% colnames(outcomes)
+  ## debug
+  print(c(goodstate,goodoutcome))
 }
